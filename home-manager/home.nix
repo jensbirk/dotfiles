6d9bae1f63fbb7c084a8fb6f928b3340b200fb37
Jens Birk <jens@birk.one>
2026-07-01 20:35:49 +0200

    Revert "Add ghostty font config and starship spotify module"
    This reverts commit 59972138ab712ca2c21dc00c0e85a7ba1fd865ad.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -20,36 +20,6 @@ in
 
   fonts.fontconfig.enable = true;
 
-  home.file."config/ghostty/config".text = ''
-    font-family = JetBrainsMono Nerd Font
-    font-size = 12
-    window-decoration = false
-    window-padding-x = 12
-    window-padding-y = 12
-    background-opacity = 1.0
-    background-blur-radius = 32
-    cursor-style = block
-    cursor-style-blink = true
-    scrollback-limit = 3023
-    mouse-hide-while-typing = true
-    copy-on-select = false
-    confirm-close-surface = false
-    app-notifications = no-clipboard-copy,no-config-reload
-    keybind = ctrl+shift+n=new_window
-    keybind = ctrl+t=new_tab
-    keybind = ctrl+plus=increase_font_size:1
-    keybind = ctrl+minus=decrease_font_size:1
-    keybind = ctrl+zero=reset_font_size
-    unfocused-split-opacity = 0.7
-    unfocused-split-fill = #44464f
-    gtk-titlebar = false
-    shell-integration = detect
-    shell-integration-features = cursor,sudo,title,no-cursor
-    keybind = shift+enter=text:\n
-    gtk-single-instance = true
-    theme = dankcolors
-  '';
-
   programs.git = {
     enable = true;
     settings = {
@@ -61,15 +31,6 @@ in
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
-    settings = {
-      format = "$all";
-      spotify = {
-        format = "via [$symbol $player]($style) ";
-        symbol = "";
-        style = "dimmed white";
-        disabled = false;
-      };
-    };
   };
 
   programs.zoxide = {
