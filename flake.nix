b1e129112d06af26fc05c22b1346826be84c3ecb
Jens Birk <jens@birk.one>
2026-07-01 20:44:41 +0200

    flake: add devShell for direnv

--- flake.nix
+++ flake.nix
@@ -13,8 +13,18 @@
   outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }:
     let
       system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          git
+          nixd
+          nixfmt
+          statix
+        ];
+      };
+
       nixosConfigurations.jensnixos = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
